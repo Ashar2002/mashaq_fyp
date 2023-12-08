95ff85c1d20c4f307066465cbaa0efd55dda824c
Ashar2002 <79855121+Ashar2002@users.noreply.github.com>
2023-12-09 04:45:03 +0500

    main changes

--- src/pages/product/[...id].js
+++ src/pages/product/[...id].js
@@ -9,25 +9,29 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const InnerProduct = () => {
-  const [products,setProducts] = useState(null);
+  const [products, setProducts] = useState(null);
   const router = useRouter();
   const { id } = router.query;
   const getData = async () => {
     try {
-      const res = await axios.get(`http://localhost:5000/product/details/${id}`);
+      if (!id) return; // Skip the API call if id is not available
+      const res = await axios.get(
+        `http://localhost:5000/product/details/${id}`
+      );
       setProducts(res?.data?.data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     getData();
-  }, []);
+  }, [id]); // Trigger the effect when id changes
 
   return (
     <div>
       <Sidebar bg="bg-bisque-0" />
-      <Product products={products} />
+      {products ? <Product products={products} /> : <p>Loading...</p>}
       {/* <Recommendations /> */}
       <Footer />
     </div>
